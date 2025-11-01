3b040974d61f88c199b8aa41482e7f90e0a94a99
Yongjie Xie 谢师傅 <xieyongjie2012@163.com>
2025-11-01 17:35:32 -0400

    Update Home component layout and content; refine Main component styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,83 @@
 
 export default function Home() {
   return (
-    <div>你好</div>
+    <div className=" space-y-5">
+      <div className=" bg-gray-200 h-20"></div>
+      <div className=" border">
+        <p>子账号1</p>
+        <div className=" grid grid-cols-2 gap-10">
+          <div className=" grid grid-cols-6 gap-10">
+            <div className=" col-span-2 bg-gray-100">
+              <div className=" flex justify-between">
+                <p>角色</p>
+                <p>角色1</p>
+              </div>
+              <div className=" flex justify-between">
+                <p>等级</p>
+                <p>60</p>
+              </div>
+              <div className=" flex justify-between">
+                <p>职业</p>
+                <p>战士</p>
+              </div>
+              <div className=" flex justify-between">
+                <p>阶级</p>
+                <p>一转</p>
+              </div>
+              <div className=" flex justify-between">
+                <p>备忘</p>
+                <p>xxxxxxx</p>
+              </div>
+            </div>
+            <div className=" col-span-4">
+              <p>物品</p>
+              <div className=" flex flex-wrap gap-3">
+                <div className="badge badge-soft badge-primary">魔泉</div>
+                <div className="badge badge-soft badge-secondary">声望</div>
+                <div className="badge badge-soft badge-accent">血瓶</div>
+                <div className="badge badge-soft badge-primary">料理</div>
+                <div className="badge badge-soft badge-primary">猫头</div>
+                <div className="badge badge-soft badge-success">打卡器</div>
+              </div>
+            </div>
+          </div>
+          <div className=" grid grid-cols-6 gap-10">
+            <div className=" col-span-2 bg-gray-100">
+              <div className=" flex justify-between">
+                <p>角色</p>
+                <p>角色1</p>
+              </div>
+              <div className=" flex justify-between">
+                <p>等级</p>
+                <p>60</p>
+              </div>
+              <div className=" flex justify-between">
+                <p>职业</p>
+                <p>战士</p>
+              </div>
+              <div className=" flex justify-between">
+                <p>阶级</p>
+                <p>一转</p>
+              </div>
+              <div className=" flex justify-between">
+                <p>备忘</p>
+                <p>xxxxxxx</p>
+              </div>
+            </div>
+            <div className=" col-span-4">
+              <p>物品</p>
+              <div className=" flex flex-wrap gap-3">
+                <div className="badge badge-soft badge-primary">魔泉</div>
+                <div className="badge badge-soft badge-secondary">声望</div>
+                <div className="badge badge-soft badge-accent">血瓶</div>
+                <div className="badge badge-soft badge-primary">料理</div>
+                <div className="badge badge-soft badge-primary">猫头</div>
+                <div className="badge badge-soft badge-success">打卡器</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- components/Main.tsx
+++ components/Main.tsx
@@ -2,7 +2,7 @@
 
 export const Main = ({ children }: { children: React.ReactNode }) => {
     return (
-        <main className=" mx-15 my-5 min-h-90vh border">
+        <main className=" mx-15 my-5 min-h-[87vh]">
             {children}
         </main>
     );
